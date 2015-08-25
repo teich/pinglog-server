5906eff289658dcfd96766cd434381af2b819e3d
Oren Teich <oren@teich.net>
2015-08-25 12:03:17 -0700

    whitespace bug

--- config/prod.exs
+++ config/prod.exs
@@ -19,49 +19,15 @@ config :pinglog_server, PinglogServer.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :pinglog_server, PinglogServer.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [port: 443,
-#               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
-#
-# Where those two env variables return an absolute path to
-# the key and cert in disk or a relative path inside priv,
-# for example "priv/ssl/server.key".
-#
-# We also recommend setting `force_ssl`, ensuring no data is
-# ever sent via http, always redirecting to https:
-#
-#     config :pinglog_server, PinglogServer.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
-
-# ## Using releases
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start the server for all endpoints:
-#
-#     config :phoenix, :serve_endpoints, true
-#
-# Alternatively, you can configure exactly which server to
-# start per endpoint:
-#
-#     config :pinglog_server, PinglogServer.Endpoint, server: true
-#
-
 # Configure secret_key_base for cookie-based session storage
-  config :my_app, PinglogServer.Endpoint,
+config :pinglog_server, PinglogServer.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-  config :my_app, PinglogServer.Repo,
+config :pinglog_server, PinglogServer.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   size: 20 # The amount of database connections in the pool
+
+url: [scheme: "https", host: "enigmatic-ocean-6979.heroku.com", port: 443],
+force_ssl: [rewrite_on: [:x_forwarded_proto]],
